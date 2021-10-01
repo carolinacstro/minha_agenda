9c53229568bd566df7a00dea1957003b72075353
Carolina Castro <carolinacoliveira@outlook.com>
2021-10-01 15:09:43 -0300

    primeiro commit

---
+++ app/models/contato.rb
@@ -0,0 +1,4 @@
+class Contato < ActiveRecord::Base
+    validates_presence_of :nome, :message => " - Deve ser preenchido"
+    validates_presence_of :telefone, :message => " - Deve ser preenchido"
+  end
